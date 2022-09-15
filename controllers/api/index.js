71b3da117462d5149646160d475d1e0e74ed3262
kristafrancis721 <kristafrancis08@yahoo.com>
2022-09-15 14:06:28 -0400

    moved files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,6 @@ const router = require('express').Router;
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
-const { Router } = require('express');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
