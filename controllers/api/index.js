816287766da8023a2ee502b5bf5120b96458e440
Andrea Jared <adj0803@gmail.com>
2022-09-15 21:24:43 -0400

    commit

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,9 @@
-const router = require('express').Router;
+const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
-<<<<<<< HEAD
 
-=======
->>>>>>> 6974602ed191bcacc091ea4465829c862d22193e
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
