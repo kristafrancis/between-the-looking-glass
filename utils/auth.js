c59686637ee58cbcb26f20d099a462b72ecf7dae
Andrea Jared <adj0803@gmail.com>
2022-09-18 18:05:18 -0400

    added authorization and image files

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,16 @@
 const withAuth = (req, res, next) => {
-    if (!req.session.user_id) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
+  if (!req.session.user_id) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
+
+
+
+
+
+
+
