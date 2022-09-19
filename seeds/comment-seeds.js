f6ab918e325eee84c1dae14761af7ea287a58f36
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-09-18 21:12:33 -0400

    seeds2

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,42 +2,42 @@ const { Comment } = require('../models');
 
 const commentdata = [
   {
-    comment_text: 'https://www.indeed.com/viewjob?jk=75e683ae770d8a30&tk=1gd8r7048gaht805&from=serp&vjs=3',
+    comment_text: 'Nunc rhoncus dui vel sem.',
     user_id: 6,
     post_id: 1
   },
   {
-    comment_text: 'https://www.indeed.com/viewjob?jk=afc76d28a728844f&tk=1gd8r7048gaht805&from=serp&vjs=3',
+    comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
     user_id: 6,
     post_id: 8
   },
   {
-    comment_text: 'https://www.indeed.com/viewjob?cmp=Full-Circle-Financial-Services&t=Junior+Developer&jk=da216014640823da&q=junior+developer&vjs=3',
+    comment_text: 'Aliquam erat volutpat. In congue.',
     user_id: 3,
     post_id: 10
   },
   {
-    comment_text: 'Cipher Tech Solutions is looking for a Junior Level Android level Reverse Engineer. If you are good with mobile devices this could be a great opportunity for you. Its worth doing the research.',
+    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
     user_id: 3,
     post_id: 18
   },
   {
-    comment_text: 'Just went to an interview with Amazon, it seems like a toxic environment and talk about diversity but dont uphold themselves to this standard.',
+    comment_text: 'In hac habitasse platea dictumst.',
     user_id: 7,
     post_id: 5
   },
   {
-    comment_text: 'Are you good with game programming? there is a job posting for a junior software engineer in tampa.https://www.indeed.com/viewjob?jk=bb9a6b766ca4d285&tk=1gd8r7048gaht805&from=serp&vjs=3',
+    comment_text: 'Vivamus vestibulum sagittis sapien.',
     user_id: 1,
     post_id: 20
   },
   {
-    comment_text: 'EASY INTERVIEW QUESTION GOT HARDER: check out this link for some interview questions:https://stackoverflow.com/questions/3492302/easy-interview-question-got-harder-given-numbers-1-100-find-the-missing-numbe ',
+    comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
     user_id: 6,
     post_id: 7
   },
   {
-    comment_text: 'Friend had an interview at Google today, I thought this tricky question was interesting and i am having some issues wrapping my head around it. Does anyone have any thoughts? Follow link to stackoverflow. https://stackoverflow.com/questions/5505894/tricky-google-interview-question',
+    comment_text: 'Sed vel enim sit amet nunc viverra dapibus.',
     user_id: 7,
     post_id: 4
   },
@@ -54,7 +54,214 @@ const commentdata = [
   {
     comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
     user_id: 3,
-   }
+    post_id: 14
+  },
+  {
+    comment_text: 'Donec ut mauris eget massa tempor convallis.',
+    user_id: 5,
+    post_id: 4
+  },
+  {
+    comment_text:
+      'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+    user_id: 4,
+    post_id: 9
+  },
+  {
+    comment_text:
+      'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
+    user_id: 5,
+    post_id: 14
+  },
+  {
+    comment_text: 'Quisque porta volutpat erat.',
+    user_id: 6,
+    post_id: 2
+  },
+  {
+    comment_text: 'Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
+    user_id: 8,
+    post_id: 2
+  },
+  {
+    comment_text:
+      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
+    user_id: 2,
+    post_id: 20
+  },
+  {
+    comment_text: 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',
+    user_id: 4,
+    post_id: 11
+  },
+  {
+    comment_text:
+      'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
+    user_id: 5,
+    post_id: 13
+  },
+  {
+    comment_text:
+      'Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
+    user_id: 9,
+    post_id: 16
+  },
+  {
+    comment_text: 'Curabitur convallis.',
+    user_id: 6,
+    post_id: 4
+  },
+  {
+    comment_text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    user_id: 4,
+    post_id: 10
+  },
+  {
+    comment_text: 'Morbi non quam nec dui luctus rutrum.',
+    user_id: 3,
+    post_id: 8
+  },
+  {
+    comment_text:
+      'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique.',
+    user_id: 8,
+    post_id: 10
+  },
+  {
+    comment_text:
+      'Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.',
+    user_id: 1,
+    post_id: 15
+  },
+  {
+    comment_text: 'Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend.',
+    user_id: 5,
+    post_id: 3
+  },
+  {
+    comment_text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    user_id: 1,
+    post_id: 15
+  },
+  {
+    comment_text: 'Nam tristique tortor eu pede.',
+    user_id: 4,
+    post_id: 16
+  },
+  {
+    comment_text: 'Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
+    user_id: 4,
+    post_id: 18
+  },
+  {
+    comment_text: 'Proin eu mi. Nulla ac enim.',
+    user_id: 4,
+    post_id: 10
+  },
+  {
+    comment_text: 'Sed ante. Vivamus tortor.',
+    user_id: 7,
+    post_id: 5
+  },
+  {
+    comment_text: 'Aliquam quis turpis eget elit sodales scelerisque.',
+    user_id: 10,
+    post_id: 1
+  },
+  {
+    comment_text: 'Donec quis orci eget orci vehicula condimentum.',
+    user_id: 3,
+    post_id: 19
+  },
+  {
+    comment_text: 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    user_id: 5,
+    post_id: 3
+  },
+  {
+    comment_text: 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.',
+    user_id: 10,
+    post_id: 14
+  },
+  {
+    comment_text: 'Maecenas ut massa quis augue luctus tincidunt.',
+    user_id: 10,
+    post_id: 8
+  },
+  {
+    comment_text: 'Cras in purus eu magna vulputate luctus.',
+    user_id: 10,
+    post_id: 11
+  },
+  {
+    comment_text: 'Etiam vel augue. Vestibulum rutrum rutrum neque.',
+    user_id: 8,
+    post_id: 5
+  },
+  {
+    comment_text: 'Proin at turpis a pede posuere nonummy.',
+    user_id: 8,
+    post_id: 19
+  },
+  {
+    comment_text: 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.',
+    user_id: 9,
+    post_id: 19
+  },
+  {
+    comment_text:
+      'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
+    user_id: 5,
+    post_id: 4
+  },
+  {
+    comment_text:
+      'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.',
+    user_id: 2,
+    post_id: 11
+  },
+  {
+    comment_text: 'Vestibulum ac est lacinia nisi venenatis tristique.',
+    user_id: 4,
+    post_id: 6
+  },
+  {
+    comment_text: 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    user_id: 9,
+    post_id: 6
+  },
+  {
+    comment_text:
+      'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam.',
+    user_id: 7,
+    post_id: 9
+  },
+  {
+    comment_text: 'Integer ac leo. Pellentesque ultrices mattis odio.',
+    user_id: 4,
+    post_id: 19
+  },
+  {
+    comment_text: 'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
+    user_id: 10,
+    post_id: 1
+  },
+  {
+    comment_text:
+      'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.',
+    user_id: 2,
+    post_id: 19
+  },
+  {
+    comment_text: 'Proin risus. Praesent lectus.',
+    user_id: 10,
+    post_id: 1
+  },
+  {
+    comment_text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus.',
+    user_id: 10,
+    post_id: 12
+  }
 ];
 
 const seedComments = () => Comment.bulkCreate(commentdata);
